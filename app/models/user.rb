1d1caa1954d70647233b522ac9d42f6f254772de
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-28 14:47:02 +0200

    Separate scss files

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 50 }
   validates :fullname, presence: true , length: { minimum: 6, maximum: 50 }
 
-  has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id'
+  has_many :opinions, class_name: 'Opinion', foreign_key: 'author_id', dependent: :destroy
 
   # These are in the followings in the followings table
-  has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
+  has_many :followings, class_name: 'Following', foreign_key: 'follower_id', dependent: :destroy
   # These are the users
-  has_many :followed_persons, through: :followings, source: :followed
+  has_many :followed_persons, through: :followings, source: :followed, dependent: :destroy
   # These are in the followings in the followings table
-  has_many :following_folks, class_name: 'Following', foreign_key: 'followed_id'
+  has_many :following_folks, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy
 
   has_many :followers, through: :following_folks, source: :follower
 
