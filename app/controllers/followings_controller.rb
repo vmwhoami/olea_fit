4ac0e28add3b5167f792daee7131f4da0b28aef3
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-29 09:38:13 +0200

    Add follow olny unfollowed users with logic in views

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,6 +2,7 @@ class FollowingsController < ApplicationController
   
   def create
      current_user.followings.create(followed_id: params[:user_id])
+     redirect_back(fallback_location: root_path)
   end
 
   def destroy
