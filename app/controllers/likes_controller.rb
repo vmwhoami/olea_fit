a6ce51a4eec48d3684b1e92bc44db3e21a542397
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-02 11:48:01 +0200

    Solve the like controller issue

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,29 +3,28 @@
 class LikesController < ApplicationController
   def create
     current_user.likes.create(opinion_id: params[:opinion_id])
-    return_back
+   return_back
   end
 
   # /opinions/:opinion_id/likes/:id(.:format)
   def destroy
     @like = current_user.likes.find(params[:id])
     @like.destroy
-    return_back
+   return_back
   end
 
   def destroy
     like = Like.find_by(id: params[:id], user: current_user, opinion_id: params[:opinion_id])
     if like
       like.destroy
-      return_back
+     return_back
     else
-      return_back
+     return_back
     end
   end
   private 
-
   def return_back
-    return_back
+    redirect_back(fallback_location: root_path)
   end
     
 end
