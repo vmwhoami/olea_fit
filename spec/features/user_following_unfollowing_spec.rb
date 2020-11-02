d9809206ce5c6559b9f5c3149151b972d8c38bcf
Vitalie Melnic <vmwhoami@gmail.com>
2020-11-02 22:51:37 +0200

    fix rubocop errors

--- spec/features/user_following_unfollowing_spec.rb
+++ spec/features/user_following_unfollowing_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
+
 RSpec.feature 'UserFollowingUnfollowings', type: :feature do
   before(:each) do
     @test_one = User.create(username: 'test_one', fullname: 'Test UserOne')
     @test_two = User.create(username: 'test_two', fullname: 'Test UserTwo')
     @test_three = User.create(username: 'test_three', fullname: 'Test UserThree')
-  end
-
-  scenario 'User follows another user' do
     visit '/'
     fill_in 'Username', with: 'test_one'
     click_button 'Login'
+  end
+
+  scenario 'User follows another user' do
     expect(page).to have_link('Test UserTwo')
     expect(page).to have_link('Test UserOne')
     expect(page).to have_link('Test UserThree')
   end
 
   scenario 'There should be a button to follow another user' do
-    visit '/'
-    fill_in 'Username', with: 'test_one'
-    click_button 'Login'
     click_link 'Test UserTwo'
     expect(page).to have_link('Follow')
     click_link 'Follow'
@@ -28,9 +26,6 @@ RSpec.feature 'UserFollowingUnfollowings', type: :feature do
   end
 
   scenario 'There should be a button to unfollow another user' do
-    visit '/'
-    fill_in 'Username', with: 'test_one'
-    click_button 'Login'
     click_link 'Test UserTwo'
     click_link 'Follow'
     click_link 'Unfollow'
