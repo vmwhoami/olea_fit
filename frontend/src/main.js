4eef0050f40a2f1a7709a2427f9d97cdac778b36
vmwhoami <vmwhoami@gmail.com>
2025-02-18 19:44:23 +0200

    add pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia'
 import App from "./App.vue";
-import routes from "@/router/index.js";
+import router from "@/router/index.js";
+import "./styles.css";
 
-createApp(App).use(routes).mount("#app");
+const pinia = createPinia()
+const app = createApp(App);
+app.use(pinia)
+app.use(router);
+app.mount("#app");
