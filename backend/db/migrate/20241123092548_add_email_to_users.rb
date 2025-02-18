218a87a73f200ec7ee256be723ce9b7a13a68758
vmwhoami <vmwhoami@gmail.com>
2025-02-18 14:03:46 +0200

    add  mandatory email for users

--- backend/db/migrate/20241123092548_add_email_to_users.rb
+++ backend/db/migrate/20241123092548_add_email_to_users.rb
@@ -1,6 +1,6 @@
-class AddEmailToUsers < ActiveRecord::Migration[7.2]
+class AddEmailToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :email, :string
+    add_column :users, :email, :string, null: false
     add_index :users, :email, unique: true
   end
 end
