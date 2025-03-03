db7b5f7d0c8b25a4ebe1920bc03ed2a5ec10532c
vmwhoami <vmwhoami@gmail.com>
2025-03-03 17:52:27 +0200

    add reference design

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-         <icon-olea-fit-logo />  
+         <icon-olea-fit-logo  myFill="#b4b4b4"/>  
       </a>
       <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
