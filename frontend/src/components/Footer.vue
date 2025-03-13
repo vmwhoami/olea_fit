bd9206b7d0b32ef0964603cc9f2c8fcadcc7e9c4
vmwhoami <vmwhoami@gmail.com>
2025-03-13 23:02:20 +0200

    add current year to footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,16 +1,20 @@
 <template>
-      <!-- Footer -->
-      <footer class="bg-white border-t border-gray-200 py-6">
-      <div class="container mx-auto px-6 text-center">
-        <!-- Menu links -->
-        <ul class="flex justify-center space-x-4 mb-4 text-gray-600 text-sm md:text-base">
-          <li><a href="#" class="hover:text-gray-900">Home Page</a></li>
-          <li><a href="#" class="hover:text-gray-900">Class Time</a></li>
-          <li><a href="#" class="hover:text-gray-900">Review</a></li>
-          <li><a href="#" class="hover:text-gray-900">Last Post</a></li>
-          <li><a href="#" class="hover:text-gray-900">Contact</a></li>
-        </ul>
-        <p class="text-gray-500 text-sm">&copy;2021 AvidWorkouts. All rights reserved.</p>
-      </div>
-    </footer>
+  <footer class="bg-white border-t border-gray-200 py-6">
+    <div class="container mx-auto px-6 text-center">
+      <ul class="flex justify-center space-x-4 mb-4 text-gray-600 text-sm md:text-base">
+        <li><a href="#" class="hover:text-gray-900">Home Page</a></li>
+        <li><a href="#" class="hover:text-gray-900">Class Time</a></li>
+        <li><a href="#" class="hover:text-gray-900">Review</a></li>
+        <li><a href="#" class="hover:text-gray-900">Last Post</a></li>
+        <li><a href="#" class="hover:text-gray-900">Contact</a></li>
+      </ul>
+      <p class="text-gray-500 text-sm">
+        &copy;vmwhoami {{ currentYerar }}. All rights reserved.
+      </p>
+    </div>
+  </footer>
 </template>
+
+<script setup>
+const currentYerar = new Date().getFullYear();
+</script>
