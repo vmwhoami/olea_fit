5879b094d77e9107713a639c6d1e6feef0258bad
vmwhoami <vmwhoami@gmail.com>
2025-03-21 15:35:26 +0200

    update package json add my cv as html

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="flex-no-wrap fixed top-0 flex w-full items-center py-2 dark:shadow-black/10 lg:flex-wrap lg:py-4"
+    class="fixed top-0 flex w-full items-center py-2 dark:shadow-black/10 lg:flex-wrap lg:py-4"
   >
     <router-link
       to="/"
